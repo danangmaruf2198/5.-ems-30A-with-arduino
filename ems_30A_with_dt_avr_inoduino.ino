98ff64f462a7e5fd3e18c0c642826ef082dc705b
Danang Ma'ruf <48882391+danangmaruf2198@users.noreply.github.com>
2020-01-14 20:57:20 +0700

    Add files via upload

---
+++ ems_30A_with_dt_avr_inoduino.ino
@@ -0,0 +1,29 @@
+
+int min1 = 23; //aktifkan motor1 port 1
+int min2 = 25; //aktifkan motor1 port 2
+int men1=27; //port 3 enable  motor
+int men2=29;//port 4 enable motor
+int pwminput=3; //pwm input
+
+void setup(){ 
+    pinMode(min1, OUTPUT);   
+    pinMode(min2, OUTPUT);
+    pinMode(men1, OUTPUT);   
+    pinMode(men2, OUTPUT);
+    pinMode(pwminput, OUTPUT);   
+
+} 
+
+void loop (){
+  int value;
+  for(value = 0 ; value <= 255; value+=5){
+    digitalWrite(min1,HIGH);   
+    digitalWrite(min2, LOW);
+    digitalWrite(men1,HIGH);   
+    digitalWrite(men2, HIGH);
+    analogWrite(pwminput,value);   
+    delay(500);
+  }   
+}
+
+
